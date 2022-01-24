79feb91b630fa3493761b36a9186003a9d230d72
Giraffbutler <97141544+Giraffbutler@users.noreply.github.com>
2022-01-24 17:40:43 -0500

    Added "has ingredient" function to test for component

--- Functions.py
+++ Functions.py
@@ -1,2 +1,14 @@
 def initialize():
     print('Welcome to the EasyShare digital recipe recording/sharing tool!')
+
+def menu():
+    return input('''What would you like to do?
+    
+    1. Enter a new recipe
+    2. Edit an existing recipe
+    3. Delete an existing recipe
+    
+    ''')
+
+def enter_recipe():
+    
---
+++ Portfolio_project.py
@@ -0,0 +1,8 @@
+### Initialization
+from Functions import *
+from Classes import *
+import time
+
+initialize()
+time.sleep(1)
+action = menu()
