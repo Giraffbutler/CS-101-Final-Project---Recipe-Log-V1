720a5916872748947048326349729530bb28a05c
Giraffbutler <97141544+Giraffbutler@users.noreply.github.com>
2022-01-25 14:21:03 -0500

    Completed Program draft 1 w/ entering, editing, storing, deleting, checking ingredients for allergen information

--- Classes.py
+++ Classes.py
@@ -6,24 +6,16 @@ class Recipe:
     def __repr__(self):
         return f'This recipe contains infomation on how to make {self.name}.'
 
-    def edit_name(self,new_name):
-        self.name = new_name
-
     def get_ingredients(self):
         pass
 
-    def has_ingredient(self,ingredients):
-        if type(ingredients) is str:
-            if ingredients in self.ingredients:
-                print(f'The recipe does contain {ingredients}')
-            else:
-                print(f'The recipe does not contain {ingredients}')
-        elif type(ingredients) is list:
-            contained_ingredients = []
+    def has_ingredients(self):
+        ingredients = input('Input any ingredients you want to search for separated by a space\n').split()
+        with open(self.recipe_file, 'r') as current:
+            current = current.read().upper()
             for ingredient in ingredients:
-                for item in self.ingredients:
-                    if ingredient in item:
-                        contained_ingredients.append(ingredient)
-            print('The recipe contains the following ingredients:')
-            for element in contained_ingredients:
-                print(element)
+                if ingredient.upper() in current:
+                    print(f'The recipe does contain {ingredient}')
+                else:
+                    print(f'The recipe does not contain {ingredient}')
+        return
--- Functions.py
+++ Functions.py
@@ -12,8 +12,15 @@ def retrieve_available_recipes():
     with open('Available_recipes.txt', 'r') as recipes:
         recipes_string = recipes.read()
         for line in recipes_string.split('\n'):
-            temp = line.split('\t')
-            current_recipes.append(Recipe(temp[0], recipe_file = temp[1]))
+            try:
+                temp = line.split('\t')
+                current_recipes.append(Recipe(temp[0], recipe_file = temp[1]))
+            except:
+                add_recipe = input('There are no recipes currently logged. Would you like to add one? Y/N \n')
+                if add_recipe.upper() in ['Y','YES']:
+                    enter_recipe()
+                else:
+                    menu()
     return current_recipes
 
 def display_available_recipes(menu_return = True):
@@ -23,15 +30,18 @@ def display_available_recipes(menu_return = True):
         print(str(iter) + '. ' + recipe.name)
         iter += 1
     if menu_return == True:
-        menu()
+        wait()
 
 def enter_recipe():
     name = input('Enter a name for the recipe.\n')
     print('Select the recipe reference text file. \n')
     time.sleep(.5)
     recipe_filename = filedialog.askopenfilename(initialdir=os.getcwd())
-    with open('Available_recipes.txt', 'a') as current_recipes:
-        current_recipes.write('\n'+ name + '\t' + recipe_filename)
+    with open('Available_recipes.txt', 'r+') as current_recipes:
+        if current_recipes.read() == '':
+            current_recipes.write(name + '\t' + recipe_filename)
+        else:
+            current_recipes.write('\n'+ name + '\t' + recipe_filename)
     wait()
     
 def open_recipe():
@@ -44,23 +54,71 @@ def open_recipe():
                 with open(temp[1], 'r') as temp_2:
                     print(temp_2.read())
                     wait()
+    print('Recipe not found')
+    wait()
+
+def edit_recipe():
+    display_available_recipes(False)
+    my_recipe = input('What recipe would you like to edit?\n')
+    with open('Available_recipes.txt', 'r') as recipes:
+        for line in recipes.read().split('\n'):
+            temp = line.split('\t')
+            if my_recipe in temp[0]:
+                oscommand = 'notepad.exe ' + temp[1]
+                os.system(oscommand)
+                wait()
+    print('Recipe not found')
+    wait()
+
+def check_for_ingredient():
+    display_available_recipes(False)
+    current_recipes = retrieve_available_recipes()
+    choice = input('What recipe would you like to check for ingredients?\n')
+    for recipe in current_recipes:
+        if recipe.name == choice:
+            recipe.has_ingredients()
+    wait()
+
+def delete_recipe():
+    display_available_recipes(False)
+    temp = input('What recipe would you like to delete?\n')
+    with open('Available_recipes.txt', 'r') as recipes:
+        recipes = recipes.read().split('\n')
+        for item in recipes:
+            recipe_name_file = item.split('\t')
+            if temp in recipe_name_file[0]:
+                delete = input(f'Delete recipe {recipe_name_file[0]}? Y/N \n').upper()
+                if delete in ['Y', 'YES']:
+                    delete_line = item
+                else:
+                    wait()
+    with open('Available_recipes.txt', 'r+') as recipes_2:
+        temp = recipes_2.read().split('\n')
+        recipes_2.seek(0)
+        for line in temp:
+            if line != delete_line:
+                recipes_2.write(line)
+        recipes_2.truncate()
+    wait()
 
 def wait():
-    input()
-    menu()
-                
+    input('continue?')
+    menu()          
 
 def menu():
     choice = int(input('''What would you like to do?
     
     1. Enter a new recipe
-    2. Delete an existing recipe
-    3. Display available recipes
-    4. Open Recipe
-    5. Close program
+    2. Edit an existing recipe
+    3. Delete an existing recipe
+    4. Display available recipes
+    5. Open Recipe
+    6. Check for ingredient in recipe
+    7. Close program
+
     '''))
 
-    actions = {1: enter_recipe, 2: '', 3: display_available_recipes, 4: open_recipe, 5: exit_program}
+    actions = {1: enter_recipe,2: edit_recipe, 3: delete_recipe, 4: display_available_recipes, 5: open_recipe, 6: check_for_ingredient, 7: exit_program}
     actions[choice]()
 
 def exit_program():
